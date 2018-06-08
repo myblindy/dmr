846b6cd10c8e167ea7f669becec732a2d7b8728c
Marius Mitea <mmitea@trusimulation.com>
2018-06-08 16:04:43 -0400

    gl terrain renderer start!

---
+++ dmr.gl/Renderers/TerrainRenderer.cs
@@ -0,0 +1,19 @@
+﻿using dmr.Models.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmr.gl.Renderers
+{
+    internal class TerrainRenderer
+    {
+        private Map Map;
+
+        internal TerrainRenderer(Map map)
+        {
+            Map = map;
+        }
+    }
+}
